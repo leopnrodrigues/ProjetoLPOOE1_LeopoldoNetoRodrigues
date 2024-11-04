917ff568e730ef3ee063d51afb82933f109f6132
leopnrodrigues <leopoldorodrigues.pf030@academico.ifsul.edu.br>
2024-11-04 09:09:03 -0300

    Update Passagem.java

--- src/main/java/model/Passagem.java
+++ src/main/java/model/Passagem.java
@@ -9,6 +9,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 /**
  *
@@ -42,7 +44,7 @@ public class Passagem implements Serializable{
     public void setCabine(int cabine) {
         this.cabine = cabine;
     }
-
+    
     public Veiculo getVeiculo() {
         return veiculo;
     }
