ea28df198be89351a484a4546441b4b126d4b469
leo <leopoldorodrigues.pf030@academico.ifsul.edu.br>
2024-12-03 05:55:09 -0300

    criando telas

--- src/main/java/br/edu/ifsul/passofundo/projetolpooe1_leopoldonetorodrigues/dao/PersistenciaJPA.java
+++ src/main/java/br/edu/ifsul/passofundo/projetolpooe1_leopoldonetorodrigues/dao/PersistenciaJPA.java
@@ -4,6 +4,8 @@
  */
 package br.edu.ifsul.passofundo.projetolpooe1_leopoldonetorodrigues.dao;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -35,21 +37,41 @@ public class PersistenciaJPA implements InterfaceBD{
 
     @Override
     public void persist(Object o) throws Exception {
+        entity = getEntityManager();
         try {
-            entity = getEntityManager();
             entity.getTransaction().begin();
+            if (!entity.contains(o)) {
+                o = entity.merge(o); // Anexa o objeto ao contexto de persistência, se necessário
+            }
             entity.persist(o);
             entity.getTransaction().commit();
         } catch (Exception e) {
             if (entity.getTransaction().isActive()) {
                 entity.getTransaction().rollback();
             }
+            Logger.getLogger(PersistenciaJPA.class.getName()).log(Level.SEVERE, "Erro ao persistir a entidade: " + o.getClass().getSimpleName(), e);
+            e.printStackTrace(); // Isso imprimirá o erro completo no console
+            throw e;
         }
     }
 
     @Override
     public void remover(Object o) throws Exception {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        entity = getEntityManager();
+        try {
+            entity.getTransaction().begin();
+            if (!entity.contains(o)) {
+                o = entity.merge(o); // Garantir que o objeto está no contexto de persistência
+            }
+            entity.remove(o); // Remover o objeto
+            entity.getTransaction().commit();
+        } catch (Exception e) {
+            System.err.println("Erro ao remover entidade: " + o.getClass().getSimpleName());
+            e.printStackTrace();
+            if (entity.getTransaction().isActive()) {
+                entity.getTransaction().rollback();
+            }
+        }
     }
     
     public EntityManager getEntityManager() {
